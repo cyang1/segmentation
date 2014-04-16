3384510d693ea9a74c3d9951cb910f0c2b42dbc8
Cary Yang <caryy@cmu.edu>
2014-04-16 15:37:52 -0400

    Custom floodFill implementation, looks kinda weird

--- src/test.cpp
+++ src/test.cpp
@@ -2,13 +2,72 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <queue>
+#include <vector>
 #include <cstdint>
+
+struct LabeledComponent {
+    unsigned int id;
+    cv::Vec3b avg_color;
+};
+
+struct Point {
+    unsigned int row;
+    unsigned int col;
+};
+
+static inline bool within_delta(cv::Vec3b c1, cv::Vec3b c2, cv::Scalar delta)
+{
+    cv::Vec3b diff = c1 - c2;
+    return abs(diff[0]) < delta[0] && abs(diff[1]) < delta[1] && abs(diff[2]) < delta[2];
+}
+
+// Point is row, col
+static inline void floodFill(cv::Mat src, cv::Mat mask, LabeledComponent& cmp, Point seed, cv::Scalar diff)
+{
+    const static uint8_t NUM_DIRECTIONS = 4;
+    const static int8_t DIRECTIONS[][2] = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+
+    unsigned int num_pixels = 1;
+    cv::Vec3i total_color = src.at<cv::Vec3b>(seed.row, seed.col);
+    std::queue<Point> next_points;
+    next_points.push(seed);
+
+    unsigned char* src_data = src.data;
+    while (!next_points.empty()) {
+        Point cur_pt = next_points.front();
+        cv::Vec3b cur_color = src.at<cv::Vec3b>(cur_pt.row, cur_pt.col);
+        next_points.pop();
+
+        for (int i = 0; i < NUM_DIRECTIONS; i++) {
+            int new_row = cur_pt.row + DIRECTIONS[i][0];
+            int new_col = cur_pt.col + DIRECTIONS[i][1];
+
+            if (mask.at<uint16_t>(new_row + 1, new_col + 1) == 0) {
+                int c_1 = src_data[src.step * new_row + src.channels() * new_col];
+                int c_2 = src_data[src.step * new_row + src.channels() * new_col + 1];
+                int c_3 = src_data[src.step * new_row + src.channels() * new_col + 2];
+                cv::Vec3b next_color(c_1, c_2, c_3);
+
+                if (within_delta(cur_color, next_color, diff))
+                {
+                    total_color += next_color;
+                    num_pixels++;
+                    mask.at<uint16_t>(new_row + 1, new_col + 1) = cmp.id;
+                    next_points.push({ new_row, new_col });
+                }
+            }
+        }
+    }
+    cmp.avg_color = total_color * (1.0 / num_pixels);
+}
 
 int main()
 {
     cv::VideoCapture cap(0);
     if (!cap.isOpened())
     {
+        std::cerr << "Webcam could not be opened." << std::endl;
         return -1;
     }
     cv::namedWindow("orig");
@@ -19,8 +78,6 @@ int main()
         cv::Mat src;
         cap >> src;
         cv::resize(src, src, cv::Size(640, 480));
-        // Seems to be the most effective way to remove noise.
-        cv::medianBlur(src, src, 5);
 
         // Convert to lab color space so euclidian distance between colors
         // mimics perceptual difference.
@@ -28,18 +85,47 @@ int main()
         cv::Mat src_lab(src.size(), CV_8UC3);
         cv::cvtColor(src, src_lab, CV_BGR2Lab);
 
+        cv::Mat src_copy(src.size(), CV_8UC3);
+        cv::bilateralFilter(src_lab, src_copy, 10, 20, 5);
+        src_lab = src_copy;
+
         // Floodfill regions that are close enough in color.
-        cv::Mat mask = cv::Mat::zeros(src_lab.rows + 2, src_lab.cols + 2, CV_8U);
+        std::vector<LabeledComponent> components;
+        cv::Mat mask = cv::Mat::zeros(src_lab.rows + 2, src_lab.cols + 2, CV_16U);
+
+        // This removes the necessity to make boundary checks
+        for (int col = 0; col < mask.cols; col++) {
+            // Set to MAX_INT
+            mask.at<uint16_t>(0, col) = (uint16_t)-1;
+            mask.at<uint16_t>(mask.rows - 1, col) = (uint16_t)-1;
+        }
+
+        for (int row = 0; row < mask.rows; row++) {
+            // Set to MAX_INT
+            mask.at<uint16_t>(row, 0) = (uint16_t)-1;
+            mask.at<uint16_t>(row, mask.cols - 1) = (uint16_t)-1;
+        }
 
         for (int row = 0; row < src_lab.rows; ++row) {
-            auto src_lab_begin = src_lab.ptr<uint8_t>(row);
+            auto mask_begin = mask.ptr<uint16_t>(row + 1);
             for (int col = 0; col < src_lab.cols; ++col) {
                 // Pixel at row, col = Mask at row + 1, col + 1
-                if (mask.at<uint8_t>(row + 1, col + 1) == 0) {
-                    cv::Scalar_<uint8_t> pixel(src_lab_begin[0], src_lab_begin[1], src_lab_begin[2]);
-                    cv::floodFill(src_lab, mask, cv::Point(col, row), pixel, NULL, cv::Scalar(3, 2, 2), cv::Scalar(3, 2, 2));
+                if (mask_begin[col + 1] == 0) {
+                    LabeledComponent cmp;
+                    cmp.id = components.size() + 1;
+                    floodFill(src_lab, mask, cmp, { row, col }, cv::Scalar(3, 1, 1));
+                    components.push_back(cmp);
                 }
-                src_lab_begin += 3;
+            }
+        }
+
+        for (int row = 0; row < src.rows; ++row) {
+            auto mask_ptr = mask.ptr<uint16_t>(row + 1);
+            auto src_ptr = src_lab.ptr<cv::Vec3b>(row);
+            for (int col = 0; col < src.cols; ++col) {
+                int cmp_id = mask_ptr[col + 1];
+                if (cmp_id > 0)
+                    src_ptr[col] = components[cmp_id - 1].avg_color;
             }
         }
 
