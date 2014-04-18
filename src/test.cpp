10fdfb4e0882024b2be6a80ed06798bba1f08c96
Cary Yang <caryy@cmu.edu>
2014-04-17 22:51:14 -0400

    Fixed flood fill, made another window.

--- src/test.cpp
+++ src/test.cpp
@@ -18,8 +18,10 @@ struct Point {
 
 static inline bool within_delta(cv::Vec3b c1, cv::Vec3b c2, cv::Scalar delta)
 {
-    cv::Vec3b diff = c1 - c2;
-    return abs(diff[0]) <= delta[0] && abs(diff[1]) <= delta[1] && abs(diff[2]) <= delta[2];
+    int d_a = c1[0] - c2[0];
+    int d_b = c1[1] - c2[1];
+    int d_c = c1[2] - c2[2];
+    return abs(d_a) <= delta[0] && abs(d_b) <= delta[1] && abs(d_c) <= delta[2];
 }
 
 // Point is row, col
@@ -62,9 +64,57 @@ static inline void floodFill(cv::Mat src, cv::Mat mask, LabeledComponent& cmp, P
     cmp.avg_color = total_color * (1.0 / num_pixels);
 }
 
+static inline cv::Mat segment(cv::Mat bgr) {
+    cv::Mat yuv(bgr.size(), CV_8UC3), segmented(bgr.size(), CV_8UC3);
+    cv::cvtColor(bgr, yuv, CV_BGR2YCrCb);
+
+    for (int i = 0; i < bgr.rows; ++i) {
+        for (int j = 0; j < bgr.cols; ++j) {
+            auto pixel = yuv.data + i*yuv.step + j*yuv.channels();
+            auto seg_pixel = segmented.data + i*segmented.step + j*segmented.channels();
+            int y = pixel[0], v = pixel[1], u = pixel[2];   //y, cb, cr
+            if ((y >= 32 && y < 64 && u >= 136) || //blue
+                (y >= 64 && y < 144 && u >= 144))
+            {
+                seg_pixel[0] = 255;
+                seg_pixel[1] = 0;
+                seg_pixel[2] = 0;
+            }
+            else if ((y < 32 && u < 144 && v < 120) ||  //green
+                (y >= 32 && y < 96 && v < 120) ||
+                (y >= 96 && y < 144 && v < 104))
+            {
+                seg_pixel[0] = 0;
+                seg_pixel[1] = 255;
+                seg_pixel[2] = 0;
+            }
+            else if (v >= 144)  //red
+            {
+                seg_pixel[0] = 0;
+                seg_pixel[1] = 0;
+                seg_pixel[2] = 255;
+            }
+            else
+            {
+                seg_pixel[0] = 255;
+                seg_pixel[1] = 255;
+                seg_pixel[2] = 255;
+            }
+            if (y <= 48)        //black
+            {
+                seg_pixel[0] = 0;
+                seg_pixel[1] = 0;
+                seg_pixel[2] = 0;
+            }
+
+        }
+    }
+    return segmented;
+}
+
 int main()
 {
-    cv::VideoCapture cap(0);
+    cv::VideoCapture cap(1);
     if (!cap.isOpened())
     {
         std::cerr << "Webcam could not be opened." << std::endl;
@@ -72,7 +122,9 @@ int main()
     }
     cv::namedWindow("orig");
     cv::namedWindow("filled");
+    cv::namedWindow("canny");
     cv::namedWindow("segmented");
+    cv::namedWindow("filled + segmented");
     for (;;)
     {
         cv::Mat src;
@@ -89,6 +141,8 @@ int main()
         cv::bilateralFilter(src_lab, src_copy, 10, 20, 5);
         src_lab = src_copy;
 
+        cv::cvtColor(src_lab, src, CV_Lab2BGR);
+
         cv::Mat canny;
         cv::Canny(src_lab, canny, 40, 90);
         canny.convertTo(canny, CV_16U, 256);
@@ -97,7 +151,6 @@ int main()
         std::vector<LabeledComponent> components;
         cv::Mat mask = cv::Mat::zeros(src_lab.rows + 2, src_lab.cols + 2, CV_16U);
 
-
         cv::Mat tmp = mask.clone();
         cv::Mat roi(tmp(cvRect(1, 1, src.cols, src.rows)));
         canny.copyTo(roi);
@@ -105,11 +158,11 @@ int main()
         mask |= canny;
 
         for (int row = 0; row < mask.rows; row++) {
+            auto mask_begin = mask.ptr<uint16_t>(row);
             for (int col = 0; col < mask.cols; col++) {
-                auto pixel = mask.at<uint16_t>(row, col);
-                if (pixel != 0 || row == 0 || col == 0 ||
+                if (mask_begin[col] != 0 || row == 0 || col == 0 ||
                     row == mask.rows - 1 || col == mask.cols - 1) {
-                    pixel = (uint16_t)-1;
+                    mask_begin[col] = (uint16_t)-1;
                 }
             }
         }
@@ -131,64 +184,24 @@ int main()
             auto mask_ptr = mask.ptr<uint16_t>(row + 1);
             auto src_ptr = src_lab.ptr<cv::Vec3b>(row);
             for (int col = 0; col < src.cols; ++col) {
-                int cmp_id = mask_ptr[col + 1];
+                short cmp_id = mask_ptr[col + 1];
                 if (cmp_id > 0)
                     src_ptr[col] = components[cmp_id - 1].avg_color;
             }
         }
 
         // Convert back.
-        cv::Mat dest(src.size(), CV_8UC3), yuv(src.size(), CV_8UC3), segmented(src.size(), CV_8UC3);
+        cv::Mat dest(src.size(), CV_8UC3);
         cv::cvtColor(src_lab, dest, CV_Lab2BGR);
-        cv::cvtColor(dest, yuv, CV_BGR2YCrCb);
 
-        for (int i = 0; i < src.rows; ++i) {
-            for (int j = 0; j < src.cols; ++j) {
-                auto pixel = yuv.data + i*yuv.step + j*yuv.channels();
-                auto seg_pixel = segmented.data + i*segmented.step + j*segmented.channels();
-                int y = pixel[0], v = pixel[1], u = pixel[2];   //y, cb, cr
-                if ((y >= 32 && y < 64 && u >= 136) || //blue
-                         (y >= 64 && y < 144 && u >= 144))
-                {
-                    seg_pixel[0] = 255;
-                    seg_pixel[1] = 0;
-                    seg_pixel[2] = 0;
-                }
-                else if ((y < 32 && u < 144 && v < 120) ||  //green
-                         (y >= 32 && y < 96 && v < 120) ||
-                         (y >= 96 && y < 144 && v < 104))
-                {
-                    seg_pixel[0] = 0;
-                    seg_pixel[1] = 255;
-                    seg_pixel[2] = 0;
-                }
-                else if (v >= 144)  //red
-                {
-                    seg_pixel[0] = 0;
-                    seg_pixel[1] = 0;
-                    seg_pixel[2] = 255;
-                }
-                else
-                {
-                    seg_pixel[0] = 255;
-                    seg_pixel[1] = 255;
-                    seg_pixel[2] = 255;
-                }
-                if (y <= 48)        //black
-                {
-                    seg_pixel[0] = 0;
-                    seg_pixel[1] = 0;
-                    seg_pixel[2] = 0;
-                }
-
-            }
-        }
-
+        cv::Mat orig_segmented = segment(src);
+        cv::Mat segmented = segment(dest);
 
         imshow("orig", src);
         imshow("filled", dest);
-        imshow("segmented", segmented);
         imshow("canny", canny);
+        imshow("segmented", orig_segmented);
+        imshow("filled + segmented", segmented);
         if (cv::waitKey(30) >= 0)
         {
             break;
